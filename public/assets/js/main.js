69620efa304be6d1853af9f8c3034bc5c2331165
Yigit <71589865+Yigit-Findik@users.noreply.github.com>
2021-03-07 15:34:34 +0100

    a

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,14 +1,18 @@
 function numberCheck() {
-    
+    housenmb = parseInt(document.getElementById("housenmb").value);
+    //var housenmb = document.getElementById("housenmb");
+    if(isNaN(housenmb)){
+        alert("fill in a number!");
+    }
 }
 
 function checkField() {
-    var username = document.getElementById("firstname");
+    var firstname = document.getElementById("firstname");
     var lastname = document.getElementById("lastname");
     var streetname = document.getElementById("streetname");
 
-    if(firstname.value.length == "" || lastname.value.length == "") {
-        alert("voer iets >:(");
+    if(firstname.value.length == "" || lastname.value.length == "" || streetname.value.length == "") {
+        alert("voer je voor/achter naam in en straatnaam!");
         return false;
     }
 
